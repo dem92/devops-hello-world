95b6a3788f326ffe6901f786b577ba60084cee96
Eva Dahloe <daheva15@student.westerdals.no>
2018-09-03 10:29:57 +0200

    First commit

---
+++ src/main/java/dem92/App.java
@@ -0,0 +1,17 @@
+package dem92;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(helloGenerator());
+    }
+
+    static String helloGenerator(){
+        return "Hello world!";
+    }
+}
